9329624179131782900eeb3231274d1d5b24fccf
Ajit Choudhary <ajitchoudhary8764@gmail.com>
2025-06-16 09:04:02 +0530

    Maven-Selenium-Code

--- src/test/java/maven_code2/MultipleLanguages_Google.java
+++ src/test/java/maven_code2/MultipleLanguages_Google.java
@@ -15,13 +15,13 @@ public class MultipleLanguages_Google
 	{
 		ChromeOptions options= new ChromeOptions();
 		
-		   //options.addArguments("--lang=hi");               //Hindi
+		   options.addArguments("--lang=hi");               //Hindi
 		   //options.addArguments("--lang=ar");                //Arabic
 		  // options.addArguments("--lang=ru");                  //Russian
 		  // options.addArguments("--lang=de");                  //German
 		  // options.addArguments("--lang=gu");                  //Gujrati
 		   //options.addArguments("--lang=ja");                  //Japanese
-		   options.addArguments("--lang=fr");                  //French
+		   //options.addArguments("--lang=fr");                  //French
 		   
 		   WebDriver driver= new ChromeDriver(options);
 		   
---
+++ src/test/java/maven_code3/Xpath_AxesSyntax.java
@@ -0,0 +1,46 @@
+package maven_code3;
+
+public class Xpath_AxesSyntax 
+{
+	// 1. Ancestor 
+	
+	             // TN[AN=AV]/ancestor::select
+	
+	// 2. Ancestor-or-self
+	
+	             // TN[AN=AV]/ancestor-or-self::*        (Change * if know the name)
+	
+	// 3. Descendant 
+	
+	             // Select[ID='value']/descendant::option
+	
+	// 4. Descendant-oself
+	
+                    // Select[ID='value']/descendant-or-self::option
+	
+	// 5. Child
+	
+	              // div/child::span
+	
+	// 6. Parent
+	
+	              // input/parent::div
+	
+	// 7. Following
+	
+	               // div/following::*
+	
+	// 8. Following-Sibling
+	
+                   // div/following-sibling::*
+	
+	// 9. Preceding
+	
+	               // div/preceding::*
+	
+	// 10. Preceding-sibling
+	
+	               // div/preceding-sibling::*
+
+
+}
